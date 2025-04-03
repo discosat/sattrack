a3cf0f3e1108f4b28f96d9369fea19bb84837854
Boas <boas.hermansson01@gmail.com>
2025-04-03 09:57:17 +0200

    using dummy rotor

--- app/services/rotor_controller.py
+++ app/services/rotor_controller.py
@@ -6,24 +6,13 @@ class RotorController:
 
     @classmethod
     async def initialize(cls):
-        if cls.reader is None:
-            cls.reader, cls.writer = await asyncio.open_connection("192.168.4.1", 4533)
         return cls
 
     @classmethod
     async def read(cls):
-        cls.writer.write("p".encode())
-        await cls.writer.drain()
-        response = await cls.reader.read(64)
-        response = response.decode()
-        response = response.splitlines()
-        az = response[0]
-        el = response[1]
-        return az, el
+        return 50, 50
 
 
     @classmethod
     async def write(cls, az, el):
-        cls.writer.write(f"P {az} {el}".encode())
-        await cls.writer.drain()
-        response = await cls.reader.read(64)
+        print(f"setting az:{az} el:{el}")
