3c8707e1d54be1524cb99e6d0f827cc1bccd05d5
Boas <boas.hermansson01@gmail.com>
2025-05-05 12:08:39 +0200

    added rotorcontrollor back for testing

--- app/services/rotor_controller.py
+++ app/services/rotor_controller.py
@@ -6,25 +6,25 @@ class RotorController:
 
     @classmethod
     async def initialize(cls):
-        #f cls.reader is None:
-            #cls.reader, cls.writer = await asyncio.open_connection("192.168.4.1", 4533)
+        if cls.reader is None:
+            cls.reader, cls.writer = await asyncio.open_connection("192.168.4.1", 4533)
         return None
 
     @classmethod
     async def read(cls):
-        #cls.writer.write("p".encode())
-        #await cls.writer.drain()
-        #response = await cls.reader.read(64)
-        #response = response.decode()
-        #response = response.splitlines()
-        #az = response[0]
-        #el = response[1]
-        return 1, 1
+        cls.writer.write("p".encode())
+        await cls.writer.drain()
+        response = await cls.reader.read(64)
+        response = response.decode()
+        response = response.splitlines()
+        az = response[0]
+        el = response[1]
+        return az, el
 
 
     @classmethod
     async def write(cls, az, el):
-        #cls.writer.write(f"P {az} {el}".encode())
-        ##await cls.writer.drain()
-        #response = await cls.reader.read(64)
+        cls.writer.write(f"P {az} {el}".encode())
+        #await cls.writer.drain()
+        response = await cls.reader.read(64)
         return
